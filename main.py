b23d01bf34d331416d4d4db6cdc40f774a6ce4c0
G-ENC <bartugenc@gmail.com>
2026-01-13 09:37:23 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#bot_rul:https://discord.com/oauth2/authorize?client_id=1460519577451757569&permissions=8&integration_type=0&scope=bot
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
